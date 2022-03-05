2f0ad1538f0510b41fee9017ca52ed842d50a5bd
plancer16 <2843151994@qq.com>
2022-03-05 19:41:29 +0800

    01

---
+++ README.md
@@ -0,0 +1,2 @@
+## Build up web application with Gin
+[官方地址](https://go.dev/doc/tutorial/web-service-gin)
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type album struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)//输出json
+}
+func postAlbums (c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {//请求体绑到newAlbum
+		return
+	}
+	albums = append(albums, newAlbum)//加到常量里面
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _,a := range albums{
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	}
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums",getAlbums)
+	router.GET("/albums/:id",getAlbumByID)
+	router.POST("/albums",postAlbums)
+	router.Run("localhost:8080")
+}
